989cea7e712b072724390d20b0ae9a38d8f5ab5f
paolo <paolo.casugay@gmail.com>
2024-03-21 18:06:17 +0800

    Create general Error page

--- app/error.tsx
+++ app/error.tsx
@@ -6,20 +6,24 @@ import React, { ReactNode } from 'react';
 import { Button } from '@/components';
 import styles from './App.module.css';
 
-const NotFound = (): ReactNode => {
+type Props = {
+  error: Error;
+  reset: () => void;
+};
+
+const Error = ({ error, reset }: Props): ReactNode => {
   const router = useRouter();
 
   const styleClassNames = 'py-8 flex flex-col items-center gap-8';
-  const classNames = clsx(
-    'not-found-page',
-    styleClassNames,
-    styles['not-found-page']
-  );
+  const classNames = clsx('error-page', styleClassNames, styles['error-page']);
 
   return (
     <main className={classNames}>
-      <h1 className="text-xl font-bold text-red-600">Not Found</h1>
-      <p className="text-lg">Could not find requested resource</p>
+      <h1 className="text-xl font-bold text-red-600">Error</h1>
+      <p className="text-lg">
+        {error.name}: {error.message}
+      </p>
+      <Button onClick={reset}>Try again</Button>
       <div className="flex gap-4">
         <Button onClick={() => router.push('/')}>Go to Home page</Button>
         <Button onClick={router.back}>Return to previous page</Button>
@@ -28,4 +32,4 @@ const NotFound = (): ReactNode => {
   );
 };
 
-export default NotFound;
+export default Error;
