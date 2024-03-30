4c3fce31a8a335778b8e627580e80a2b320fc95d
paolo <paolo.casugay@gmail.com>
2024-03-30 11:18:10 +0800

    Create Todo type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export { type default as Album } from './Album';
 export { type default as Post } from './Post';
+export { type default as Todo } from './Todo';
 export { type default as User } from './User';
