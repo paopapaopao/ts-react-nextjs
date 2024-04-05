3b5705230c21a62339707f657ce401c15816ec82
paolo <paolo.casugay@gmail.com>
2024-04-05 20:58:25 +0800

    Create SideBar component

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+import styles from './SideBar.module.css';
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+const SideBar = ({ children, className }: Props): ReactNode => {
+  const classNames = clsx(
+    'side-bar',
+    styles['side-bar'],
+    'sticky p-8 flex flex-col gap-8 border-r',
+    className
+  );
+
+  return <div className={classNames}>{children}</div>;
+};
+
+export default SideBar;
