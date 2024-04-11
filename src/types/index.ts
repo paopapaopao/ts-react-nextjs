fa9820e262db6be174073c9f7e5edf02ba9fad97
paolo <paolo.casugay@gmail.com>
2024-04-11 17:05:44 +0800

    Create Comment type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export { type default as Album } from './Album';
+export { type default as Comment } from './Comment';
 export { type default as Photo } from './Photo';
 export { type default as Post } from './Post';
 export { type default as Todo } from './Todo';
