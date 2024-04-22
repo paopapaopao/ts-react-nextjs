b6027df25c01d3d9f3b5ccd3c0215f6757f34a1d
paopapaopao <paolo.casugay@gmail.com>
2024-04-22 10:15:13 +0800

    Create getAlbumPhoto api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 export { getUserAlbums } from './album';
+export { getAlbumPhoto } from './photo';
 export { getPosts, getUserPosts } from './post';
 export { getUserTodos } from './todo';
 export { getUser } from './user';
---
+++ src/api/photo.ts
@@ -0,0 +1,34 @@
+import { Photo } from '@/types';
+
+const getAlbumPhoto = async (
+  albumId: string,
+  photoId: string
+): Promise<Photo> => {
+  let albumPhoto: Photo[] = [
+    {
+      albumId: -1,
+      id: -1,
+      thumbnailUrl: '',
+      title: '',
+      url: ''
+    }
+  ];
+
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/photos?albumId=${albumId}&id=${photoId}`
+    );
+
+    if (!response.ok) {
+      throw new Error('An error occurred while getting album photo.');
+    }
+
+    albumPhoto = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return albumPhoto[0];
+};
+
+export { getAlbumPhoto };
