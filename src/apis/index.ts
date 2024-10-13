d799693b7cd63194d81fc1098e3c5bff62d23a0f
paopapaopao <paolo.casugay@gmail.com>
2024-10-13 16:17:33 +0800

    Refactor getAlbumPhoto

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,5 @@
 export { getUserAlbums } from './albums';
 export {  } from './comments';
-export { getAlbumPhotos, getAlbumPhoto } from './photos';
 export { } from './posts';
 export {  } from './todos';
 export { getUser } from './users';
