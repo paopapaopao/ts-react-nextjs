e9d77d0b990c93c919eb100c53cda92cce6411ad
paopapaopao <paolo.casugay@gmail.com>
2024-10-16 22:34:39 +0800

    Add generateStaticParams function

--- src/app/profile/posts/@modal/(...)posts/[postId]/page.tsx
+++ src/app/profile/posts/@modal/(...)posts/[postId]/page.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import { type Post } from '@prisma/client';
 import { readPost } from '@/apis';
 import { Dialog, PostCard } from '@/components';
-// import { prisma } from '@/lib';
+import { prisma } from '@/lib';
 // import { type Comment } from '@/types';
+
+export async function generateStaticParams(): Promise<
+  Array<{ postId: string }>
+> {
+  const postIds = await prisma.post.findMany({
+    select: {
+      id: true
+    }
+  });
+
+  return postIds.map((postId) => ({
+    postId: postId.id.toString() // Convert to string as the route expects string params
+  }));
+}
 
 interface Props {
   params: {
