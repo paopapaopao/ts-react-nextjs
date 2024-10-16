b9fa1aa5aa92be92b24db1c93aa5cf50865b76ae
paopapaopao <paolo.casugay@gmail.com>
2024-10-16 21:51:36 +0800

    Create readPosts API

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,4 @@
 export {  } from './comments';
-export { createPost, readPost, updatePost, deletePost } from './posts';
+export { createPost, readPost, readPosts, updatePost, deletePost } from './posts';
 export {  } from './todos';
 export { getUser } from './users';
